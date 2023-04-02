dc1615c35d18f0e367ca11359aef920f8c4bb83b
nolasco-ever <74639646+nolasco-ever@users.noreply.github.com>
2023-04-02 12:36:57 -0400

    Created post card
    Created a post card with mock post data. There are 3 types:
    - Only photo post
    - Only text post
    - Photo and text post

--- FlexCoach/screens/Tabs/Social/SocialScreen.tsx
+++ FlexCoach/screens/Tabs/Social/SocialScreen.tsx
@@ -1,26 +1,55 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import React from 'react';
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import { colors } from '../../../colors';
 import { tabIcons } from '../../../components/icons/icon-library';
 import { CustomText } from '../../../components/text/customText';
+import PostCard from '../../../components/cards/postCard';
+import { photoPostMock, photoTextPostMock, textPostMock } from '../../../mocks/postMocks';
 
 
 export const SocialScreen = () => {
   const appColors = colors();
   return (
     <SafeAreaView style={[styles.container, {backgroundColor: appColors.background}]}>
-        <FontAwesomeIcon icon={tabIcons.social as IconProp} color={appColors.inactive} size={50}/>
-        <CustomText type='header' centered>Social</CustomText>
+        {/* <FontAwesomeIcon icon={tabIcons.social as IconProp} color={appColors.inactive} size={50}/>
+        <CustomText type='header' centered>Social</CustomText> */}
+        <ScrollView>          
+          <PostCard
+            postID={photoPostMock.postID}
+            profilePicture={photoPostMock.profilePicture}
+            profileName={photoPostMock.profileName}
+            postText={photoPostMock.postText}
+            postImage={photoPostMock.postImage}
+            likes={photoPostMock.likes}
+            comments={photoPostMock.comments}
+          />
+          <PostCard
+            postID={textPostMock.postID}
+            profilePicture={textPostMock.profilePicture}
+            profileName={textPostMock.profileName}
+            postText={textPostMock.postText}
+            postImage={textPostMock.postImage}
+            likes={textPostMock.likes}
+            comments={textPostMock.comments}
+          />
+          <PostCard
+            postID={photoTextPostMock.postID}
+            profilePicture={photoTextPostMock.profilePicture}
+            profileName={photoTextPostMock.profileName}
+            postText={photoTextPostMock.postText}
+            postImage={photoTextPostMock.postImage}
+            likes={photoTextPostMock.likes}
+            comments={photoTextPostMock.comments}
+          />
+        </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    flex: 1
   }
 });
