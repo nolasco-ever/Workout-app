0ca5e8e1cba5ea45c82e2951be276bbc00b2cfc1
nolasco-ever <74639646+nolasco-ever@users.noreply.github.com>
2023-04-16 15:45:48 -0400

    added under construction animation
    replaced icon to under construction animation in placeholder screen

--- FlexCoach/animations/shared/index.tsx
+++ FlexCoach/animations/shared/index.tsx
@@ -2,3 +2,4 @@ export const successCheckAnimation = require('./animations/success-check.json');
 export const successCheckAnimation2 = require('./animations/success-check-2.json');
 export const loadingSpinnerAnimation = require('./animations/loading-spinner.json');
 export const progressBarAnimation = require('./animations/progress-bar.json');
+export const underConstructionAnimation = require('./animations/under-construction.json')
--- FlexCoach/screens/placeholderScreen.tsx
+++ FlexCoach/screens/placeholderScreen.tsx
@@ -6,6 +6,8 @@ import { colors } from '../colors';
 import { tabIcons } from '../components/icons/icon-library';
 import { CustomText } from '../components/text/customText';
 import { AlertBanner } from '../components/banners/alertBanner';
+import { AnimatedImage } from '../components/utils/AnimatedImage';
+import { underConstructionAnimation } from '../animations/shared';
 
 export const PlaceholderScreen = ({ route }: {route: any}) => {
   const { title, icon } = route.params;
@@ -13,14 +15,16 @@ export const PlaceholderScreen = ({ route }: {route: any}) => {
   return (
     <SafeAreaView style={[styles.container, {backgroundColor: appColors.background}]}>
       <View style={{justifyContent: 'center', alignItems: 'center'}}>
-        <FontAwesomeIcon icon={icon as IconProp} color={appColors.inactive} size={50} style={{marginBottom: 10}}/>
+        <AnimatedImage
+          source={underConstructionAnimation}
+          size={250}
+        />
         <CustomText type='header' centered>{title}</CustomText>
       </View>
 
       <View style={{width: '100%', position: 'absolute', bottom: 10}}>
         <AlertBanner
           message='This screen is currently under construction. Check back soon! 👷‍♂️🔨'
-          type='warning'
         />
       </View>
 
